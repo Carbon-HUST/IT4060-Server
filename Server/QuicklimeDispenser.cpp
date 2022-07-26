c31bab9c259a647987478c5bf47cddf45a4d71f9
AnhNH01 <haianh552@gmail.com>
2022-07-26 15:36:18 +0700

    fix QuicklimeDispenser's constructor

--- Server/QuicklimeDispenser.cpp
+++ Server/QuicklimeDispenser.cpp
@@ -1,6 +1,6 @@
 #include "QuicklimeDispenser.h"
 
-QuicklimeDispenser::QuicklimeDispenser(int id, std::string name, std::vector<DeviceParameter> parameters)
+QuicklimeDispenser::QuicklimeDispenser(int id, std::string name, std::vector<DeviceParameter> parameters) : Device(id, name, parameters)
 {
 	SingleDeviceParameter<double> *limeAmount = new SingleDeviceParameter<double>("L", "Lime Amount", 100);
 	this->addParameter(limeAmount);
